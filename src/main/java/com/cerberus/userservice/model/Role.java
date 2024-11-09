d8f20211b9faee056cb27784e8a667f0adadaff8
egor <egormalov.2007@gmail.com>
2024-11-09 19:08:08 +1000

    №01. Создание папки model, User, Role

---
+++ src/main/java/com/cerberus/userservice/model/Role.java
@@ -0,0 +1,6 @@
+package com.cerberus.userservice.model;
+
+public enum Role {
+
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/cerberus/userservice/model/User.java
@@ -0,0 +1,37 @@
+package com.cerberus.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private Role role;
+
+}
