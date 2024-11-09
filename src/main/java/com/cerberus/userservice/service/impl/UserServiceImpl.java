1a5bcc7018e538ff51fdac22dc0ad01d016699f1
egor <egormalov.2007@gmail.com>
2024-11-09 22:58:39 +1000

    №05. Добавлен пакет exception, UserNotFoundException, пакет service, UserService, подпакет impl, UserServiceImpl. В сервисе юзера реализованы операции update, get, delete.

---
+++ src/main/java/com/cerberus/userservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.cerberus.userservice.service.impl;
+
+import com.cerberus.userservice.dto.UserDto;
+import com.cerberus.userservice.exception.UserNotFoundException;
+import com.cerberus.userservice.mapper.EntityDtoMapper;
+import com.cerberus.userservice.model.User;
+import com.cerberus.userservice.repository.UserRepository;
+import com.cerberus.userservice.service.UserService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EntityDtoMapper mapper;
+
+    @Override
+    @Transactional
+    public UserDto get(Long id) {
+        return this.mapper.toDto(this.userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id)));
+    }
+
+    @Override
+    @Transactional
+    public void update(Long id, UserDto userDto) {
+        this.userRepository.findById(id).ifPresentOrElse(user -> {
+            userDto.setId(id);
+            this.userRepository.save(this.mapper.toEntity(userDto));
+        }, () -> {
+            throw new UserNotFoundException(id);
+        });
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        this.userRepository.deleteById(id);
+    }
+}
