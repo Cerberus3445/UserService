0ab7b5786b5b9382b39024138bb88d36d63e5e87
Egor <egormalov.2007@gmail.com>
2026-02-28 20:13:54 +0300

    GP-4: User added

---
+++ src/main/java/ru/gentleman/user/entity/User.java
@@ -0,0 +1,37 @@
+package ru.gentleman.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(schema = "accounts", name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Boolean isEnabled;
+
+    private Boolean isEmailVerified;
+}
