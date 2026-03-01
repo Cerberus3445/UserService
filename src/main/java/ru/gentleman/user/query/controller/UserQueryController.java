51854ca094663f9c136ebe0dbecbed5264428751
Egor <egormalov.2007@gmail.com>
2026-03-01 21:00:17 +0300

    GP-6: UserController renamed to UserQueryController

--- src/main/java/ru/gentleman/user/query/controller/UserQueryController.java
+++ src/main/java/ru/gentleman/user/query/controller/UserQueryController.java
@@ -2,27 +2,19 @@ package ru.gentleman.user.query.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.gentleman.commom.exception.ValidationException;
-import ru.gentleman.commom.util.ValidationErrorUtils;
 import ru.gentleman.user.dto.UserDto;
 import ru.gentleman.user.service.UserService;
 
-import java.net.URI;
-import java.util.Locale;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 @Tag(name = "User Query Controller", description = "Взаимодействие с query операциями пользователя")
-public class UserController {
+public class UserQueryController {
 
     private final UserService userService;
 
